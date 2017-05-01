3a632f459dd311f541fea2104db4f3d4a246f5f3
twiw49 <twiw49@gmail.com>
2017-05-01 19:44:16 +0900

    query

--- sql/typeahead/load_data_tags.php
+++ sql/typeahead/load_data_tags.php
@@ -7,7 +7,10 @@ $sql = "SELECT * FROM `tag` ORDER BY `card_count` DESC;";
 $result = mysqli_query($conn, $sql);
 $arr = array();
 while ($row = mysqli_fetch_array($result)) {
-    array_push($arr, htmlspecialchars_decode($row['name']));
+    if ($row['category'] == 'risk_factor') {
+        $row['category'] = 'Risk Factor';
+    }
+    array_push($arr, htmlspecialchars_decode($row['name']).' / '.$row['category']);
 };
 
 echo json_encode($arr);
